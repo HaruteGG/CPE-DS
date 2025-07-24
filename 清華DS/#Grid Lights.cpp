7eeb1754e8c5852900cdee9cb30697fa13aec1e4
Harute <79552137+HaruteGG@users.noreply.github.com>
2025-07-24 12:52:34 +0800

    #Grid Lights.cpp

--- 清華DS/#Grid Lights.cpp
+++ 清華DS/#Grid Lights.cpp
@@ -1,34 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, m;
-    cin >> n >> m;
-    vector<int> row_click(n, 0);
-    vector<int> col_click(n, 0);
-    vector<vector<int>> light(n, vector<int>(n, 0));
-    for(int i=0; i<m; i++) {
-        int a, b;
-        cin >> a >> b;
-        row_click[a-1]++;
-        col_click[b-1]++;
-    }
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(row_click[i] % 2 == 0 && col_click[j] % 2 == 0) {
-                light[i][j] = 0;
-            }else if(row_click[i] % 2 == 1 && col_click[j] % 2 == 0) {
-                light[i][j] = 1;
-            }else if(row_click[i] % 2 == 0 && col_click[j] % 2 == 1) {
-                light[i][j] = 1;
-            }else {
-                light[i][j] = 0;
-            }
-            cout << light[i][j];
-            if(j < n-1) cout << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int n, q;
+	cin >> n >> q;
+	vector<int> row_click(n), col_click(n);
+	for(int i=0; i<q; i++) {
+		int r, c;
+		cin >> r >> c;
+		row_click[r-1]++;
+		col_click[c-1]++;
+	}
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			if(j != 0) cout << " ";
+			if((row_click[i] + col_click[j]) % 2 == 0) {
+				cout << 0; 
+			}else {
+				cout << 1;
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
 }
